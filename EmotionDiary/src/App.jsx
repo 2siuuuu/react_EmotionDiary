ab289ea54cf038de3726cd4f95ed094315281d7c
Isu <dltn09194141@gmail.com>
2024-08-22 18:51:40 +0900

    added naa code

--- EmotionDiary/src/App.jsx
+++ EmotionDiary/src/App.jsx
@@ -4,15 +4,25 @@ import Diary from './Pages/Diary'
 import Home from './Pages/Home'
 import New from './Pages/New'
 import NotFound from './Pages/NotFound'
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes, Link, useNavigate } from 'react-router-dom'
 
 
 
 function App() {
+  const nav = useNavigate();
+
+  const onClickButton = () => {
+    console.log("onClickButton Event");
+    nav("/new");
+  }
 
   return (
     <>
     <div>공통</div>
+
+    <button> onClick={onClickButton}
+      New 페이지로 이동
+    </button>
     <Routes>
       <Route path='/' element={<Home />}/>
       <Route path='/new' element={<New />}/>
--- README.md
+++ README.md
@@ -3,3 +3,5 @@ One bite React - Final Project
 Notion : https://iisu.notion.site/abb0ba27a0674869a605805c584e6fdf?v=500ea6bf131d46e28147d2008b58cebd&pvs=4
 
 12.3 페이지 라우팅
+
+12.4 페이지 이동 시 방법 : 1. link 2. 버튼 이벤트 (useNavigate)
